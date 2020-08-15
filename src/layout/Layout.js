6db9cb040a564d0474a2c2951e600b87b9e9655f
largiter <web.largiter@gmail.com>
2020-08-15 22:42:26 +0200

    fix

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-<<<<<<< HEAD
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
@@ -12,22 +11,6 @@ const Layout = ({ children }) => (
   </Content>
 );
 
-=======
-import smoothscroll from 'smoothscroll-polyfill';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
-
-smoothscroll.polyfill();
-
-const Layout = ({ children }) => (
-  <Content>
-    <Header />
-    {children}
-    <Footer />
-  </Content>
-);
-
->>>>>>> de2711bfc6ca794d97b94adc437fa7e444c4e6f3
 const Content = styled.div``;
 
 export default Layout;
